e63e063003b024a16dae6c7176088f74938b150d
Twumasi Duoduaa Ama <91034181+AmaTwumasi@users.noreply.github.com>
2022-07-19 21:02:25 -0700

    Update phonetics.js

--- src/phonetics.js
+++ src/phonetics.js
@@ -1,14 +1,17 @@
 import React from "react";
-import "./phonetics.css"
+import "./phonetics.css";
 
-export default function Phonetic(props) {;
+export default function Phonetic(props) {
   return (
     <div className="phonetics">
-     <em> <a href={props.phonetic.audio} target="blank">
+      <em>
         {" "}
-        Listen
-      </a>
-      {props.phonetic.text}</em>
+        <a href={props.phonetic.audio} target="blank">
+          {" "}
+          Listen
+        </a>
+        {props.phonetic.text}
+      </em>
     </div>
   );
 }
